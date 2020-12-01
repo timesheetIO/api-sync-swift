c4f3f4a883841ecaa4b03e849e19cffc55c84cfb
Florian Rauscha <florian.rauscha@gmail.com>
2020-12-01 21:42:46 +0100

    removes type alias

--- Sources/Coding.swift
+++ Sources/Coding.swift
@@ -7,7 +7,6 @@ import Foundation
 
 public protocol APIModel: Codable, Equatable { }
 
-public typealias DateDay = Date
 public typealias DateTime = Date
 public typealias File = Data
 public typealias ID = UUID
